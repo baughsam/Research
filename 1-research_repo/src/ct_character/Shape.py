2b31d9657ae93039de3c24240405fed2ca81610d
baughsam <baughs@bu.edu>
2025-12-10 23:11:52 -0500

    Created abstract Shape Class, Concrete EllipticalCylinder Class. Both has is_inside methods that return True if specified points are inside of the Shape. Created shape_test.py to test different points.

---
+++ 1-research_repo/src/ct_character/Shape.py
@@ -0,0 +1,32 @@
+# Shape.py #Configures shape of cyclinder and ellipsoid
+from abc import ABC, abstractmethod
+import numpy as np
+from dataclasses import dataclass, field
+
+
+class Shape(ABC):
+    @abstractmethod
+    def is_inside(self, x, y, z) -> bool:
+        pass
+
+@dataclass(frozen=True)
+class EllipticalCylinder(Shape):
+    axis_a: float
+    axis_b: float
+    length_c: float
+    center: np.ndarray = field(default_factory=lambda: np.array([0, 0, 0]))
+
+
+    def is_inside(self, x, y, z) -> bool:
+        # Shift to center
+        dx = x - self.center[0]
+        dy = y - self.center[1]
+        dz = z - self.center[2]
+
+        # Check length condition
+        length_check = np.abs(dz) <= self.length_c / 2
+
+        # Check radial condition
+        radial_check = (dx / self.axis_a) ** 2 + (dy / self.axis_b) ** 2 <= 1
+
+        return length_check and radial_check
