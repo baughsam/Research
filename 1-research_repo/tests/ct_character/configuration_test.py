37ad7e7bfc427b4c8f04ad1b25e1434e3e17440d
baughsam <baughs@bu.edu>
2025-12-11 22:35:25 -0500

    start of configuration tests implemented: test_volume_calculation

---
+++ 1-research_repo/tests/ct_character/configuration_test.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pytest
+from networkx.generators.lattice import grid_graph
+
+from Exciton import Configuration
+from Shape import EllipticalCylinder
+
+# Test Setup
+@pytest.fixture
+def dummy_shape():
+    """Returns valid Shape object to use in tests."""
+    return EllipticalCylinder(axis_a=10, axis_b=10, length_c=10)
+
+@pytest.fixture
+def orthogal_config(dummy_shape):
+    """Creates a 10x10x10 cubic system."""
+    lattice = np.array([[10.0, 0.0,0.0],
+                        [0.0, 10.0, 0.0],
+                        [0.0, 0.0, 10.0]])
+    grid = (10, 10, 10)
+    # 1 atom at origin
+    atoms_pos = np.zeros((1,3))
+    atoms_type = np.array([1])
+
+    return Configuration(
+        lattice_vectors=lattice,
+        origin=np.zeros(3),
+        grid_shape = grid,
+        shape = dummy_shape,
+        atom_positions= atoms_pos,
+        atom_types = atoms_type
+    )
+
+def test_volume_calculation(orthogonal_config):
+    """Test 1: Correst volume size calculation."""
+    # 10 * 10 * 10 = 1000.0
+    assert np.isclose(orthogonal_config.volume(), 1000.0)
