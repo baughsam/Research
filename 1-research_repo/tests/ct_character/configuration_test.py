fbd14648304396e2ec2ac45f095e5f101c931e41
baughsam <baughs@bu.edu>
2025-12-12 09:53:56 -0500

    configuration tests implemented: test_voxel_calculator test_transform_matrix test_orthoganality test_immutability test_grid_safety

--- 1-research_repo/tests/ct_character/configuration_test.py
+++ 1-research_repo/tests/ct_character/configuration_test.py
@@ -1,3 +1,4 @@
+from dataclasses import FrozenInstanceError
 import numpy as np
 import pytest
 from ct_character.Exciton import Configuration
@@ -29,7 +30,60 @@ def orthogonal_config(dummy_shape):
         atom_types = atoms_type
     )
 
+# Property methods tests
 def test_volume_calculation(orthogonal_config):
     """Test 1: Correst volume size calculation."""
     # 10 * 10 * 10 = 1000.0
     assert np.isclose(orthogonal_config.total_volume, 1000.0)
+
+def test_voxel_calculation(orthogonal_config):
+    """Test 2: Correct voxel size calculation."""
+    assert np.isclose(orthogonal_config.dv, 1)
+
+def test_transform_matrix(orthogonal_config):
+    """Test 3: Correct transformation matrix."""
+    expected_matrix = np.array([[1.0, 0.0, 0.0],
+                                [0.0, 1.0, 0.0],
+                                [0.0, 0.0, 1.0]])
+    assert np.allclose(orthogonal_config.transform_matrix, expected_matrix)
+
+# Safety Tests
+
+#Orthogonality ~ Non-Cubic Structures are Common, we need to make sure they work for our code
+def test_orthoganality(dummy_shape):
+    """
+    SAFETY: Test a tilted (monoclinic) cell.
+    Simple volume = L*W*H fails here, so this proves that our cross-product math works.
+    """
+    # A box tilted 45 degrees in X-Y plane
+    lattice_non_orth = np.array([
+                        [10.0, 0.0, 0.0],   # Vector A (Length 10)
+                        [10.0, 10.0, 0.0],  # Vector B (Length 14.1, Height 10)
+                        [0.0, 0.0, 10.0]])  # Vector C (Height 10)
+
+    # Volume should be Base * Height * Depth = 10 * 10 * 10 = 1000
+    # (The tilt shouldn't change the volume of this specific shape)
+
+    config = Configuration(lattice_non_orth, np.zeros(3), (10, 10, 10),
+                           dummy_shape, np.zeros((1, 3)), np.array([1]))
+
+    assert np.isclose(config.total_volume, 1000.0)
+
+def test_immutability(orthogonal_config):
+    """
+     SAFETY: Ensure Solver cannot accidentally change the lattice.
+    """
+
+    with pytest.raises(FrozenInstanceError):
+        orthogonal_config.lattice_vectors = np.random.rand(3) * 50.0
+
+def test_grid_safety(dummy_shape):
+    """
+    Ensure grid dimensions are physically possible
+    """
+    lattice = np.eye(3)
+    bad_grid = (10, -5, 10)
+
+    with pytest.raises(ValueError, match="positive"):
+        Configuration(lattice, np.zeros(3), bad_grid,
+                      dummy_shape, np.zeros((1, 3)), np.array([1]))
