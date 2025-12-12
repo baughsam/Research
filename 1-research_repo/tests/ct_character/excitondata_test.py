a1a7630aa8e577dcd9f61832c345b1a6ef27d799
baughsam <baughs@bu.edu>
2025-12-12 14:09:05 -0500

    excitondata tests implemented: test_exciton_data_defaults test_exciton_data_mutability

---
+++ 1-research_repo/tests/ct_character/excitondata_test.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pytest
+from ct_character.Exciton import ExcitonData
+
+
+def test_exciton_data_defaults():
+    """Test 1: Correct default data"""
+
+    # Setup: Create data with just a dummy density
+    dummy_density = np.zeros((10, 10, 10))
+    data = ExcitonData(density=dummy_density)
+
+    # Test: Inputs should be stored
+    assert np.array_equal(data.density, dummy_density)
+
+    # Test: Results should be empty (Safety Check)
+    assert data.density_inside_shape is None
+    assert data.density_distance is None
+    assert data.ct_ratio is None
+    assert data.dipole_moment is None
+    assert data.quadrupole_moment is None
+    assert data.rdf_distance is None
+    assert data.rdf_values is None
+
+def test_exciton_data_mutability():
+    """Test 2: Mutability status of ExcitonData"""
+    dummy_density = np.zeros((10, 10, 10))
+    data = ExcitonData(density=dummy_density)
+    data.ct_ratio = 1
+
+    assert data.ct_ratio == 1
