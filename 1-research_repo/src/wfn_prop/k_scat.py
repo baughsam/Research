73afa3a14638fd1187193d223139b97efcf2443f
baughsam <baughs@bu.edu>
2026-06-08 16:06:26 -0400

    Transitioned the project to a disk-decoupled pipeline, separating the heavy DFT/GW physics extraction from the fast RK4 time-stepping solver.
    * OOP Encapsulation: Implemented the 'two_state_transition_matrix' dataclass in `k_scat.py`. The class automatically constructs the static N_Q x N_Q operator matrix upon instantiation and handles the dynamic RK4 step using highly optimized `np.einsum` tensor multiplication.
    * Payload Architecture: Refactored `main.py` to bypass raw physics generation. The driver now strictly loads a compiled `compiled_scat_rates_data.npz` file, allowing for rapid iteration of the spatial grid and integration steps without re-running the HDF5 extractions.
    * Pre-compiled Physics: Updated the extraction script to dynamically locate the Gamma point and I plan on inplementing pre-calculation of the analytical radiative decay rate (fs^-1), baking all necessary physical constants directly into the .npz payload.

--- 1-research_repo/src/wfn_prop/k_scat.py
+++ 1-research_repo/src/wfn_prop/k_scat.py
@@ -64,3 +64,66 @@ class PhononScat(Kscat):
         scattered_array = np.einsum('ij, xyi -> xyj', self.transition_matrix, array_3d)
 
         return scattered_array
+
+@dataclass
+class two_state_transition_matrix(Kscat):
+    """
+    Constructs the scattering matrix from first principles
+    Based on the 2024 paper: Phonon-Driven Femtosecond
+    "Dynamics of Excitons in Crystalline Penacene from First Principles"
+    """
+    # Shape: (N_Q, N_Q)
+    k_BB: np.ndarray
+    k_BD: np.ndarray
+    gamma_decay_constant: float
+    map_Q_to_q: np.ndarray
+    gamma_index: int
+    transition_matrix: np.ndarray = field(init=False)
+
+    def __post_init__(self):
+        """
+        Builds the static K_scat matrix from the provided raw physics arrays.
+        """
+        self.transition_matrix = self._build_operator
+
+    def __build_operator(self) -> np.ndarray:
+        # Check matrix sizes
+        N_Q, N_q = self.k_BB.shape
+
+        assert self.k_BB.shape == self.k_BD.shape, f"FATAL: Dimension mismatch between k_BB {self.k_BB.shape} and k_BD {self.k_BD.shape}"
+        assert self.map_Q_to_q == (N_Q, N_q), "FATAL: Mapping array shape does not match the rate arrays."
+
+        print(f"Inititalizing two_state_transition_matrix operator of size ({N_Q}, {N_Q})...")
+
+        # Initialize empty matrix
+        K_scat = np.zeros((N_Q, N_Q))
+
+        # Build diagonal (Losses)
+        sum_k_BB = np.sum(self.k_BB, axis=1)
+        sum_k_BD = np.sum(self.k_BD, axis=1)
+
+        # Build off-diagonals (Gains)
+        for i in range(N_Q):
+            for j in range(N_q):
+                rate = self.k_BB[i,j]
+                Q_final = self.map_Q_to_q[i, j]
+                K_scat[Q_final, i] += rate
+
+        # Put diagonal in K_scat
+        for i in range(N_Q):
+            K_scat[i,i] = - (sum_k_BB[i] + sum_k_BD[i])
+
+        # Insert radiative decay at gamma
+        K_scat[self.gamma_index, self.gamma_index] -= self.gamma_decay_constant
+
+        return K_scat
+
+    def calc_scattering(self, array_3d: np.ndarray) -> np.ndarray:
+        """
+        Executes the scattering step during the RK4 loop using Einstein summation.
+        """
+        if array_3d is None:
+            raise ValueError("Error: 'array_3d' is empty.")
+
+        scattered_array = np.einsum('ij, xyi -> xyj', self.transition_matrix, array_3d)
+        return scattered_array
