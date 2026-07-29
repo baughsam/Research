d6fa25ae7d56e8dbf40414c0f9126377d88565cd
baughsam <baughs@bu.edu>
2026-07-29 15:23:20 -0400

    benign update

--- 1-research_repo/src/wfn_prop/processing/gif_from_npz.py
+++ 1-research_repo/src/wfn_prop/processing/gif_from_npz.py
@@ -1,9 +1,9 @@
 import numpy as np
 import h5py
-from wfn_prop.analysis import export_diffusion_gif_updated
+from wfn_prop.analysis import export_diffusion_gif_updated1, export_diffusion_gif_updated2
 
 # 1. Open the HDF5 file in read-only mode
-h5_filename = "../LiF_8x8x8_Kscat-on_intraband-only_Q0000_initialization_S0_100fs_frames.h5"
+h5_filename = "../LiF_8x8x8_Kscat-on_intraband-only_spread_initialization_S0_200fs_frames.h5"
 h5_file = h5py.File(h5_filename, 'r')
 
 # 2. Extract the frames dataset pointer (This does NOT load it into RAM)
@@ -16,7 +16,7 @@ physics = np.load("../compiled_scat_rates_data_S0_S1_8x8x8.npz")
 q_vectors = physics['Qpts']
 
 # 4. Generate the two-panel animated GIF
-export_diffusion_gif_updated(
+export_diffusion_gif_updated2(
     frames=frames,
     dt=dt,
     save_interval=2,
@@ -27,7 +27,7 @@ export_diffusion_gif_updated(
     right_panel_mode='qgrid',
     q_vectors=q_vectors,
     projection=('x', 'y'),
-    filename="test1_LiF_8x8x8_Kscat-on_intraband-only_Q0000_initialization_S0_100fs.gif"
+    filename="test2_LiF_8x8x8_Kscat-on_intraband-only_spread_initialization_S0_200fs.gif"
 )
 
 # 5. Cleanly close the file when the animation is finished baking
