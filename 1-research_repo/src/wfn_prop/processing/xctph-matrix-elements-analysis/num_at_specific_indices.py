f976d8bd71a2d1b1032402915dd4338cbbfa1246
baughsam <baughs@bu.edu>
2026-08-03 10:42:53 -0400

    update so that Qpts and qpts prints fully for long lists

--- 1-research_repo/src/wfn_prop/processing/xctph-matrix-elements-analysis/num_at_specific_indices.py
+++ 1-research_repo/src/wfn_prop/processing/xctph-matrix-elements-analysis/num_at_specific_indices.py
@@ -1,12 +1,12 @@
 import h5py
-import numpy as np
 
-"""prefix = "8x8x8"""
 xctph_h5 = "../../xctph_4x4x4.h5"
-"""log_file_Qpts = f"{prefix}_Qpts"
+
+prefix = "8x8x8"
+log_file_Qpts = f"{prefix}_Qpts"
 log_file_qpts = f"{prefix}_qpts"
 log_file_freq = f"{prefix}_freq"
-log_file_energies = f"{prefix}_energies"""
+log_file_energies = f"{prefix}_energies"
 
 state_1 = 0
 state_2 = 2
@@ -26,15 +26,17 @@ with h5py.File(xctph_h5, mode='r') as f:
 num = g_tensor[state_1, state_2, Q_state, phonon_mode, q_state]
 print(f"xctph scattering matrix elements at ({state_1}, {state_2}, {Q_state}, {phonon_mode}, {q_state}): {num}")
 
-"""with open(log_file_Qpts, "w") as log:
+with open(log_file_Qpts, "w") as log:
     log.write("Qpts\n")
-    log.write(str(Qpts))
+    for item in Qpts:
+        log.write(f"{item}\n")
 with open(log_file_qpts, "w") as log:
     log.write("qpts\n")
-    log.write(str(qpts))
+    for item in qpts:
+        log.write(f"{item}\n")
 with open(log_file_freq, "w") as log:
     log.write("frequencies\n")
     log.write(str(freq))
 with open(log_file_energies, "w") as log:
     log.write("energies\n")
-    log.write(str(energies))"""
+    log.write(str(energies))
