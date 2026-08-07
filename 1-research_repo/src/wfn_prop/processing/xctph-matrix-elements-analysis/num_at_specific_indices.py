6b6a484f2312456f00c64d93ff7772a954b4c388
baughsam <baughs@bu.edu>
2026-08-07 11:17:34 -0400

    misc changes

--- 1-research_repo/src/wfn_prop/processing/xctph-matrix-elements-analysis/num_at_specific_indices.py
+++ 1-research_repo/src/wfn_prop/processing/xctph-matrix-elements-analysis/num_at_specific_indices.py
@@ -1,18 +1,20 @@
 import h5py
+import numpy as np
 
 xctph_h5 = "../../xctph_8x8x8.h5"
 
-prefix = "8x8x8"
+"""prefix = "2x2x2"
 log_file_Qpts = f'{prefix}_Qpts'
 log_file_qpts = f'{prefix}_qpts'
 log_file_freq = f'{prefix}_freq'
-log_file_energies = f'{prefix}_energies'
+log_file_energies = f'{prefix}_energies'"""
 
 state_1 = 0
-state_2 = 1
-Q_state =4
+state_2 = 0
+Q_state = 4
+phonon_mode = 3
 q_state = 0
-phonon_mode = 4
+
 
 
 with h5py.File(xctph_h5, mode='r') as f:
@@ -24,9 +26,11 @@ with h5py.File(xctph_h5, mode='r') as f:
 
 
 num = g_tensor[state_1, state_2, Q_state, phonon_mode, q_state]
+num_abs = np.abs(num) #** 2
 print(f"xctph scattering matrix elements at ({state_1}, {state_2}, {Q_state}, {phonon_mode}, {q_state}): {num}")
+print(f"xctph scattering matrix elements squared at ({state_1}, {state_2}, {Q_state}, {phonon_mode}, {q_state}): {num_abs}")
 
-with open(log_file_Qpts, "w") as log:
+"""with open(log_file_Qpts, "w") as log:
     log.write("Qpts\n")
     for item in Qpts:
         log.write(f"{item}\n")
@@ -41,4 +45,4 @@ with open(log_file_freq, "w") as log:
 with open(log_file_energies, "w") as log:
     log.write("energies\n")
     for item in energies:
-        log.write(f"{item}\n")
+        log.write(f"{item}\n")"""
